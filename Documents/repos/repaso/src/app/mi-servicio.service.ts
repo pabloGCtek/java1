67f3f2c6424ae72b60c732db3989950ce350b2ae
pabloGCtek <130450619+pabloGCtek@users.noreply.github.com>
2023-05-11 17:40:29 +0200

    Delete Documents directory

--- Documents/repos/repaso/src/app/mi-servicio.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class MiServicioService {
-  textoejemplo:string = "Ejemplo de texto de un servicio"
-  constructor() { }
-}
